21bbf22ef1182f4800ef0eba16e09f7be6b59177
Moruan06 <moruan.mattar@gmail.com>
2025-07-16 17:19:00 -0300

    Uso de slider para definir o tamanho dos pixeis e adição deles no grid da tela

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const canvas = document.querySelector("#container");
-let size = prompt("Insira o tamanho da tela: ");
-
 function createPixel(grid){
+    canvas.innerHTML = '';
     for(let i = 0; i < grid * grid; i++){
         const colorDiv = document.createElement("div");
         colorDiv.style.cssText = `flex-basis: ${100 / grid}%;
-        height: ${100 / grid}%;`
-        colorDiv.classList.add("pixel")
-        canvas.appendChild(colorDiv)
-    }
-}
+        height: ${100 / grid}%;`;
+        colorDiv.classList.add("pixel");
+        canvas.appendChild(colorDiv);
+    };
+};
 
-createPixel(+size)
+const gridSlid = document.querySelector("#gridSizeSlider");
+
+gridSlid.addEventListener('change', (e) => createPixel(+e.target.value));
