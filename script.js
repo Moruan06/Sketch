d468c6a8e65404b8f5fdeaa968ac0a9277ee7508
Moruan06 <moruan.mattar@gmail.com>
2025-07-16 17:03:24 -0300

    Lógica para criar os pixeis do tamanho certo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const canvas = document.querySelector("#container");
+let size = prompt("Insira o tamanho da tela: ");
 
+function createPixel(grid){
+    for(let i = 0; i < grid * grid; i++){
+        const colorDiv = document.createElement("div");
+        colorDiv.style.cssText = `flex-basis: ${100 / grid}%;
+        height: ${100 / grid}%;`
+        colorDiv.classList.add("pixel")
+        canvas.appendChild(colorDiv)
+    }
+}
+
+createPixel(+size)
